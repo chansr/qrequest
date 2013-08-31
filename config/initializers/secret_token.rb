30a6a9df64844ccf67308222ede80e45aeb145fa
Chan Mete <chansr@me.com>
2013-08-31 22:06:02 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qrequest::Application.config.secret_key_base = 'c01b63ada2447160ffd3f7215dda6338ba313270a85f87f16249c516761cd8c38ec8a1c27981fa0de0046ec2e0c70145ac0922d1014f3e70059fb6b69b4817e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Qrequest::Application.config.session_store :cookie_store, key: '_qrequest_session'
